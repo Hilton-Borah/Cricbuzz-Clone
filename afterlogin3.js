c2d3216950298dee85e3fe2712ac79172d471ca1
Hilton Borah <hiltonborah123@gmail.com>
2022-08-26 01:57:44 +0530

    day3

---
+++ afterlogin3.js
@@ -0,0 +1,42 @@
+import appendsecond from "./componanats/secondnav.js"
+
+
+let hi_container = document.getElementById("hi_second_navbar_inside")
+
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '1d4ebfd689mshd6d5452c5d03785p10425fjsnc78cb22ea40b',
+        'X-RapidAPI-Host': 'unofficial-cricbuzz.p.rapidapi.com'
+    }
+};
+
+fetch('https://unofficial-cricbuzz.p.rapidapi.com/matches/list?matchState=recent', options)
+    // .then(response => response.json())
+    // .then(response => console.log(response))
+    // .catch(err => console.error(err));
+    // .then(response => response.json())
+    // .then(response =>console.log(response); console.log(response.typeMatches[0]))
+    // .catch(err => console.error(err));
+    .then((res) => {
+        return res.json()
+    }).then((res) => {
+        console.log(res)
+        console.log(res.typeMatches[0].seriesAdWrapper[0])
+        // appenddata(res.typeMatches[0].seriesAdWrapper[0])
+        appendsecond(res.typeMatches[0].seriesAdWrapper[0],hi_container)
+        console.log(res.typeMatches[1].seriesAdWrapper[0])
+        // appenddata(res.typeMatches[1].seriesAdWrapper[0])
+        appendsecond(res.typeMatches[1].seriesAdWrapper[0],hi_container)
+        console.log(res.typeMatches[2].seriesAdWrapper[0])
+        // appenddata(res.typeMatches[2].seriesAdWrapper[0])
+        appendsecond(res.typeMatches[2].seriesAdWrapper[0],hi_container)
+        console.log(res.typeMatches[3].seriesAdWrapper[0])
+        // appenddata(res.typeMatches[3].seriesAdWrapper[0])
+        appendsecond(res.typeMatches[3].seriesAdWrapper[0],hi_container)
+    }).catch((err) => {
+        console.log(err)
+    })
+
+
