c8b42b4b3e0daf7046df96eadb91d43a1c93941c
Rohini Shinde <shinderohini34@gmail.com>
2022-08-27 22:54:55 +0530

    file added

--- schedule.js
+++ schedule.js
@@ -7,8 +7,6 @@ let data;
 const options = {
 	method: 'GET',
 	headers: {
-		//'X-RapidAPI-Key': '9867463a05msh43e5b08d97f3488p1dd403jsn16c7c6af4393',
-		//'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
         'X-RapidAPI-Key': '6d8b7919e4mshfc83a22c29e1fcbp19d801jsn3943031b23e9',
 		'X-RapidAPI-Host': 'cricbuzz-cricket.p.rapidapi.com'
 	}
